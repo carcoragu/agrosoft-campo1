b0f346b852aa919f0fb8b3d8544c72648fbd3fa2
carcoragu <87833281+carcoragu@users.noreply.github.com>
2026-03-15 16:15:15 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,36 @@
-const CACHE="agrosoft-v1"
+const CACHE_NAME = "agrosoft-cache-v2";
 
-self.addEventListener("install",e=>{
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.json"
+];
 
-e.waitUntil(
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
 
-caches.open(CACHE).then(cache=>{
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
 
-return cache.addAll([
-"/",
-"index.html",
-"style.css",
-"app.js"
-])
-
-})
-
-)
-
-})
-
-self.addEventListener("fetch",e=>{
-
-e.respondWith(
-
-caches.match(e.request).then(res=>res||fetch(e.request))
-
-)
-
-})
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
